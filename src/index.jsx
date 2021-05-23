35a7b2d2bc0b2461d24308f506aa8832dc65090f
Sarka Faloutova <sarka.faloutova@outlook.cz>
2021-05-23 13:05:27 +0200

    creating npx

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => <div className="container">Kytičky to be 🌷</div>;
+
+render(<App />, document.querySelector('#app'));
