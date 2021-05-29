0130e4c563522492eb6cca9ccdc7be6d14b2135b
Sarka Faloutova <sarka.faloutova@outlook.cz>
2021-05-29 22:39:31 +0200

    upload Foto

--- src/Wishlist/index.jsx
+++ src/Wishlist/index.jsx
@@ -1,29 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
-
-import { db } from './../firebase';
-
-import { Router, Route, NavLink, Switch } from 'react-router-dom';
+import { db, storage } from './../firebase';
 
 const Wishlist = () => {
-  const handleClick = () => {
-    db.collection('cities')
-      .doc('LA')
-      .set({
-        name: 'Los Angeles',
-        state: 'CA',
-        country: 'USA',
-      })
-      .then(() => {
-        console.log('Document successfully written!');
-      })
-      .catch((error) => {
-        console.error('Error writing document: ', error);
-      });
+  const [filename, setFilename] = useState('Pridejte kyku do wishlistu...');
+  const [fileUrl, setFileUrl] = useState(null);
+  const [users, setUsers] = useState([]);
 
-    console.log('zaznam pridan');
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    const storageRef = storage.ref();
+    const fileRef = storageRef.child(file.name);
+    fileRef.put(file).then(() => {
+      console.log('Uploaded file', file.name);
+    });
+
+    setFilename(`Nahraná kytka: ${e.target.value.split('\\').pop()}`);
   };
-  return <button onClick={handleClick}>Pridej zaznam do db</button>;
+
+  //   const username = e.target.username.value;
+  //   // if (!username) {
+  //   //   return;
+  //   // }
+  //   db.collection('users')
+  //     .doc(username)
+  //     .set({ name: username, avatar: fileUrl });
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const usersCollection = await db.collection('users').get();
+  //     setUsers(
+  //       usersCollection.docs.map((doc) => {
+  //         return doc.data();
+  //       }),
+  //     );
+  //   };
+  //   fetchUser(), [];
+  // });
+  // .then(() => {
+  //   console.log('Uploaded file', file.name);
+  //       });
+  return (
+    <>
+      <input
+        type="file"
+        name="file"
+        id="file"
+        className="inputfile"
+        onChange={onFileChange}
+      />
+      <label for="file">
+        <div>{filename}</div>
+
+        <img src="/assets/cross.svg" alt="" />
+      </label>
+
+      <br />
+      <br />
+      <div>
+        {/* <ul>
+          {users.map((user) => {
+            return (
+              <li>
+                <img width="100" height="100" src={user.avatar} alt="" />
+                <p>{user.name}</p>
+              </li>
+            );
+          })}
+        </ul> */}
+      </div>
+    </>
+  );
 };
 
 export default Wishlist;
